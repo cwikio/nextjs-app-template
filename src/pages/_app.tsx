ab3fd4fcaa4f0dd0f07175e4d8fd67af08044be4
Tom Ash <54542292+cwikio@users.noreply.github.com>
2023-11-20 19:26:03 -0500

    clerk themes installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import { type AppType } from "next/app";
-
+import type { AppType } from "next/app";
 import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+//INFO: tailwind UI configuration: https://clerk.com/docs/components/customization/overview#using-tailwind
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
       <Component {...pageProps} />;
     </ClerkProvider>
   )
